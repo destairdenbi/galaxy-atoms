77ca41eae27c484c9d865f27bd4a835b9b6aaaf2
koriege <koriege@gmail.com>
2017-10-05 17:14:08 +0200

    adapted setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/env bash
 dir=$(cd $(dirname $0) && pwd)
 for i in *.yaml; do
-	ln -sfn $i $GALAXY_ROOT/config/plugins/tours/$(basename $i)
+	ln -sfn $dir/$i $GALAXY_ROOT/config/plugins/tours/$(basename $i)
 done
 
+[[ ! -e $GALAXY_ROOT/config/galaxy.ini ]] && cp $GALAXY_ROOT/config/galaxy.ini.sample $GALAXY_ROOT/config/galaxy.ini
 sed -i -r 's/^\s*#+(\s*tour_config_dir.+)/\1/' $GALAXY_ROOT/config/galaxy.ini
 
-source $GALAXY_ROOT/.venv/bin/activate
-pip install planemo bioblend
-planemo serve --galaxy_root=$GALAXY_ROOT
-deactivate
+$GALAXY_ROOT/run.sh
+# source $GALAXY_ROOT/.venv/bin/activate
+# pip install planemo bioblend
+# planemo serve --galaxy_root=$GALAXY_ROOT
+# deactivate
